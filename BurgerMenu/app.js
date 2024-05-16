3085c7f293be59a631c785244b505fc301962034
Tovkes Anastasiia <ntovkez@gmail.com>
2024-05-16 22:11:52 +0300

    updated burger menu

--- BurgerMenu/app.js
+++ BurgerMenu/app.js
@@ -2,13 +2,13 @@ const hamMenu = document.querySelector(".ham-menu");
 
 const offScreenMenu = document.querySelector(".main-nav");
 
-// const navHeader = document.querySelector(".main-header__content")
-//
-// const navHeaderButton = document.querySelector(".main-header__content nav")
+const navHeader = document.querySelector(".main-header__content")
+
+const navHeaderButton = document.querySelector(".main-header__content nav")
 
 hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
-    // navHeader.classList.toggle("active")
-    // navHeaderButton.classList.toggle("active")
+    navHeader.classList.toggle("active")
+    navHeaderButton.classList.toggle("active")
 });
